8b525ea48e973ddd2005840d818d69997bec0205
Adrián Nieto <adrian.niezam@protonmail.com>
2026-01-29 10:54:36 +0100

    feature: rutas de fruta compleatdo

--- AnanaBanana/routes/web.php
+++ AnanaBanana/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrutaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/frutas', [FrutaController::class, 'index'])->name('fruta.all');
+    Route::get('/frutas', [FrutaController::class, 'show'])->name('fruta.details');
+    Route::get('/frutas', [FrutaController::class, 'create'])->name('fruta.create');
+    Route::post('/frutas', [FrutaController::class, 'store'])->name('fruta.store');
+    Route::get('/frutas', [FrutaController::class, 'edit'])->name('fruta.edit');
+    Route::put('/frutas', [FrutaController::class, 'update'])->name('fruta.update');
+    Route::get('/frutas', [FrutaController::class, 'destroy'])->name('fruta.destroy');
 });
 
 require __DIR__.'/auth.php';
